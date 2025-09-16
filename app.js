cda042f0ab7fa6ca006e994e59e857b70ae9660e
Andres Chiroque <107906757+s4ndrew@users.noreply.github.com>
2025-09-16 13:52:14 +0000

    modif

--- app.js
+++ app.js
@@ -1,2 +1,84 @@
-let numn = 1
-console.log(numn);
+let nombre = "Andres"
+let apellido = "Chiroque"
+
+console.log("Yo soy: " + nombre +" " + apellido)
+
+// edad
+let edad = 18
+let incrementar = 5
+let edadAños = edad + incrementar
+
+console.log("Mi edad +5: " + edadAños);
+
+//3
+let b = 7
+let h = 10
+let area = b*h/2
+
+console.log("El area es: " + area);
+
+//4
+let lado = 8
+let perimetro = 8*4
+console.log("El perimetro es:" + perimetro);
+
+//5
+let a = 6
+let i = 8
+let suma = a + i
+let resta = a - i
+let multicacion = a * i
+let divison = a / i
+let resto  = a % i
+
+console.log("Suma: " + suma);
+console.log("Resta: " + resta);
+console.log("Multiplicacion: " + multicacion);
+console.log("Divison: " + divison); 
+console.log("Resto: " + resto);
+
+
+//6
+let nota1 = 18
+let nota2 = 16
+let nota3 = 19
+let promedio = Math.round((nota1 + nota2 + nota3)/3)
+console.log("Promedio notas:" + promedio);
+
+
+//7
+console.log("Intercambio de numeros");
+let num1 = 30
+let num2 = 40
+let box = num2
+num2 = num1
+num1 = box
+console.log("Num 1 30: " + num1);
+console.log("Num 2 40: " + num2);
+
+//8
+let colorFavorito = "Me gusta el color azul"
+let miComidaFavorita = "mi comida favorita son los makis"
+console.log(colorFavorito + " y " + miComidaFavorita);
+
+//9
+let numero = 20
+let double = numero * 2
+let triple = numero * 3
+console.log("Doble: "+ double + "\n" + "Triple: " + triple);
+
+
+//9
+let precio = 100
+let descuento = (100*20)/100
+let precioFinal = precio - descuento
+
+console.log("Precio final: " + precioFinal);
+
+
+
+
+
+
+
+
